609ccd8f6cc9612c2ea178eecb1f5dcd4becbd3f
ahmedabuamra <ah.abuamra@gmail.com>
2019-03-15 00:19:16 +0200

    upgrade to ionic 3 and setting chat as home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { EmojiProvider } from '../providers/emoji';
+import { HttpClientModule } from "@angular/common/http";
+import { ChatModule } from '../pages/chat/chat.module';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+    ChatModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    EmojiProvider
   ]
 })
 export class AppModule {}
---
+++ src/pipes/pipes.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RelativeTime } from './relative-time';
+@NgModule({
+	declarations: [RelativeTime],
+	imports: [],
+	exports: [RelativeTime]
+})
+export class PipesModule {}
