d1abdcfab2186f4848490c148a61eccca867641a
karimelghazouly <karimelghazouly@gmail.com>
2019-03-16 18:41:19 +0200

    offline packages

---
+++ visuals/circle.js
@@ -0,0 +1,6 @@
+var board = JXG.JSXGraph.initBoard('box', {boundingbox: [-10, 10, 10, -10], axis:true});
+var b = JXG.JSXGraph.initBoard('box', {boundingbox: [-5, 2, 5, -2]});
+var p1 = b.createElement('point',[0,0], {name:'A',size: 4, face: 'o', fillColor:"yellow", strokeColor:"yellow"});
+var p2 = b.createElement('point',[2,-1], {name:'B',size: 4, face: 'o', fillColor:'yellow', strokeColor:"yellow"});
+var ci3 = b.create('circle',[p1,p2], 
+    {strokeWidth:1, fillColor:'#603CBA', fillOpacity:function(){ return p2.X()*0.25;} });
